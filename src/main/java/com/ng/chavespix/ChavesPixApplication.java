6c8391254172a34c16e775f9289e76bdc14ee604
Leonardo Coelho Negri <leonardocoelhonegri@gmail.com>
2022-04-16 05:22:46 -0300

    first commit

---
+++ src/main/java/com/ng/chavespix/ChavesPixApplication.java
@@ -0,0 +1,13 @@
+package com.ng.chavespix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChavesPixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChavesPixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ng/chavespix/ChavesPixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ng.chavespix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChavesPixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
