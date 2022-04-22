1027092237b628212c5b6db310998ce007a63be4
Leonardo Coelho Negri <leonardocoelhonegri@gmail.com>
2022-04-22 13:47:19 -0300

    ajuste regex

--- src/main/java/com/ng/chavespix/enums/TipoChave.java
+++ src/main/java/com/ng/chavespix/enums/TipoChave.java
@@ -5,10 +5,10 @@ import java.util.regex.Pattern;
 public enum TipoChave {
 	
 	celular("^[+][0-9]{2}[0-9]{1,3}[0-9]{9}"), 
-	email("/^[a-zA-Z0-9,]*[@][a-zA-Z0-9,]*$/{77}"), 
+	email("(^[a-z0-9]+[@][a-z0-9]+){3,77}"), 
 	cpf("^[0-9]{11}"), 
 	cnpj("^[0-9]{14}"), 
-	aleatorio("^[0-9a-z]{36}");
+	aleatorio("^[0-9a-z]{1,36}");
 	
 	private Pattern pattern;
 	
